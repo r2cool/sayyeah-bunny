d73c7daa6893634bb066d8c0856334fbd5afe748
Randy Rijkschroeff <randy@wiljekoffie.com>
2023-06-22 13:49:48 +0200

    Added code

--- sayyeah.user.js
+++ sayyeah.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Say Yeah Bunny Link Generator
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  Say Yeah bunny
 // @author       Randy Rijkschroeff
 // @updateURL    https://github.com/r2cool/sayyeah-bunny/raw/main/sayyeah.user.js
