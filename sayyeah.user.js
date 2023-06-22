612383919de9124d0283c9b276de718abcec4f54
Randy Rijkschroeff <randy@wiljekoffie.com>
2023-06-22 13:49:21 +0200

    Added code

--- sayyeah.user.js
+++ sayyeah.user.js
@@ -6,7 +6,7 @@
 // @author       Randy Rijkschroeff
 // @updateURL    https://github.com/r2cool/sayyeah-bunny/raw/main/sayyeah.user.js
 // @downloadURL  https://github.com/r2cool/sayyeah-bunny/raw/main/sayyeah.user.js
-// @match        https://dash.bunny.net/stream/126783/library/video*
+// @match        https://dash.bunny.net/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=bunny.net
 // @grant        GM_setClipboard
 // ==/UserScript==
@@ -32,14 +32,19 @@
             });
         });
     }
-    waitForElm('[label="Video ID"] input').then((elm) => {
-        var copy_button = document.createElement('button');
-        copy_button.innerText = "Kopieer link";
-        copy_button.classList.add("mt-3", "bn-button", "bn-button--lg", "bn-button__item-style--primary");
-        copy_button.setAttribute('id','but1');
-        document.querySelectorAll('[label="Video ID"]')[0].append(copy_button);
-        copy_button.addEventListener("click", () => {
-            GM_setClipboard('https://video.sayyeah.nl/' + document.querySelectorAll('[label="Video ID"] input')[0].value);
+    if (window.onurlchange === null) {
+        // feature is supported
+        window.addEventListener('urlchange', (info) => {
+            waitForElm('[label="Video ID"] input').then((elm) => {
+                var copy_button = document.createElement('button');
+                copy_button.innerText = "Kopieer link";
+                copy_button.classList.add("mt-3", "bn-button", "bn-button--lg", "bn-button__item-style--primary");
+                copy_button.setAttribute('id','but1');
+                document.querySelectorAll('[label="Video ID"]')[0].append(copy_button);
+                copy_button.addEventListener("click", () => {
+                    GM_setClipboard('https://video.sayyeah.nl/' + document.querySelectorAll('[label="Video ID"] input')[0].value);
+                });
+            });
         });
-    });
+    }
 })();
